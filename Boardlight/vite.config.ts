b2e63d61f153fdacd4e9e4add687fb090c8942a3
Menyhárt Máté <mateka.mateka07@gmail.com>
2025-07-19 09:42:09 +0200

    Fix env modifications not taking action however Vite restarts itself.

--- Boardlight/vite.config.ts
+++ Boardlight/vite.config.ts
@@ -6,7 +6,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-    process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+    const env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
     return {
         plugins: [
